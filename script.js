053ca6eabf53ef37985e489384a2d7cd5227d102
NN <43994113+NekrasovaNatalya@users.noreply.github.com>
2022-10-01 00:29:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ const sidebar = document.querySelector('.sidebar');
 const mainSlide = document.querySelector('.main-slide');
 const slidesCount = mainSlide.querySelectorAll('div').length;
 let activeSlideIndex = 0;
+
+sidebar.style.top = `-${(slidesCount - 1) * 100}vh`;
+
+upBtn.addEventListener('click', () => {
+    changeSlide('up');
+});
+
+downBtn.addEventListener('click', () => {
+    changeSlide('down');
+});
