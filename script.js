20fad3e574854e3d10dc7dfc7214ef6e12c54b6c
NN <43994113+NekrasovaNatalya@users.noreply.github.com>
2022-10-01 00:29:00 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const container = document.querySelector('.container');
+const upBtn = document.querySelector('.up-button');
+const downBtn = document.querySelector('.down-button');
+const sidebar = document.querySelector('.sidebar');
+const mainSlide = document.querySelector('.main-slide');
+const slidesCount = mainSlide.querySelectorAll('div').length;
+let activeSlideIndex = 0;
